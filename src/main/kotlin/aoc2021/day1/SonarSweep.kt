9c6e819a4c8128e592d1d5f0c1c3ea65825df5ae
stephen.dall <sdall@ctic.us>
2021-12-03 12:53:05 -0800

    Added a couple improved solutions for day 1

--- src/main/kotlin/aoc2021/day1/SonarSweep.kt
+++ src/main/kotlin/aoc2021/day1/SonarSweep.kt
@@ -1,7 +1,6 @@
 package aoc2021.day1
 
 import java.util.*
-import kotlin.collections.ArrayList
 
 class SonarSweep {
     // Count the depth increases.  Depths is the list of depts to evaluate.  Group size is how many depths
@@ -12,7 +11,7 @@ class SonarSweep {
         val previousQueue: Queue<Int> = LinkedList()
         var previousSum = 0
 
-        depths.forEach {depth ->
+        depths.forEach { depth ->
             // Make sure we have filled up the previous depth collection to the appropriate group size
             if (previousQueue.size == groupSize) {
                 // Remove the first item from the queue.
@@ -39,6 +38,57 @@ class SonarSweep {
                 // Add as we go instead of waiting for the queue to be filled.
                 previousSum += depth
             }
+        }
+
+        return count
+    }
+
+    // JetBrains solution (and James Campbell also found this).
+    fun countDepthIncreasesJetBrainsWindowed(depths: List<Int>, groupSize: Int): Int {
+        // Uses Kotlin's windowed API.
+        // windowed returns a List of a List based on group size.
+        // So, 1, 2, 3, 4, 5, 6 with a group size of 3 would be
+        // (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6)
+        return depths
+            .windowed(groupSize)
+            // This call to windowed then breaks that list into two sets, that ultimately will be
+            // ((1, 2, 3), (2, 3, 4)), ((2, 3, 4), (3, 4, 5)), ((3, 4, 5), (4, 5, 6))
+            .windowed(2).count { (a, b) ->
+                // And here we sum the two lists being compared.
+                a.sum() < b.sum()
+            }
+    }
+
+    // Inspired from an insight in the JetBrains video for this challenge.
+    // If we are comparing numbers A + B + C with B + C + D, we are really only comparing
+    // A to D.
+    fun countDepthIncreasesJetBrainsOptimization(depths: List<Int>, groupSize: Int): Int {
+        return depths
+            // With the same sample input as the previous input, we would get these windows
+            // (1, 2, 3, 4), (2, 3, 4, 5), (3, 4, 5, 6)
+            .windowed(groupSize + 1)
+            .count {
+                // And we just compare the first and last elements
+                it[0] < it[groupSize]
+            }
+    }
+
+    // Could make a case of not using .windowed at all and just indexing directly into the depths list.
+    // Certainly that would be more performant
+    // TODO this works but can be improved
+    fun countDepthIncreasesBest(depths: List<Int>, groupSize: Int): Int {
+        var count = 0
+
+        var secondIndex = groupSize
+        var firstIndex = 0
+
+        while (secondIndex < depths.size) {
+            if (depths[firstIndex] < depths[secondIndex]) {
+                count++
+            }
+
+            firstIndex++
+            secondIndex++
         }
 
         return count
--- src/test/kotlin/aoc2021/day1/SonarSweepTest.kt
+++ src/test/kotlin/aoc2021/day1/SonarSweepTest.kt
@@ -20,4 +20,53 @@ internal class SonarSweepTest {
         val sonarSweep = SonarSweep()
         assertEquals(1065, sonarSweep.countDepthIncreases(input, 3))
     }
+
+    @Test
+    fun countDepthIncreasesGroup1JetBrains() {
+        val input = PuzzleInputTestUtils.getPuzzleInputLinesIntList("day1.txt")
+
+        val sonarSweep = SonarSweep()
+        assertEquals(1121, sonarSweep.countDepthIncreasesJetBrainsWindowed(input, 1))
+    }
+
+    @Test
+    fun countDepthIncreasesGroup3JetBrains() {
+        val input = PuzzleInputTestUtils.getPuzzleInputLinesIntList("day1.txt")
+
+        val sonarSweep = SonarSweep()
+        assertEquals(1065, sonarSweep.countDepthIncreasesJetBrainsWindowed(input, 3))
+    }
+
+    @Test
+    fun countDepthIncreasesGroup1JetBrainsOptimization() {
+        val input = PuzzleInputTestUtils.getPuzzleInputLinesIntList("day1.txt")
+
+        val sonarSweep = SonarSweep()
+        assertEquals(1121, sonarSweep.countDepthIncreasesJetBrainsOptimization(input, 1))
+    }
+
+    @Test
+    fun countDepthIncreasesGroup3JetBrainsOptimization() {
+        val input = PuzzleInputTestUtils.getPuzzleInputLinesIntList("day1.txt")
+
+        val sonarSweep = SonarSweep()
+        assertEquals(1065, sonarSweep.countDepthIncreasesJetBrainsOptimization(input, 3))
+    }
+
+    @Test
+    fun countDepthIncreasesGroup1Best() {
+        val input = PuzzleInputTestUtils.getPuzzleInputLinesIntList("day1.txt")
+
+        val sonarSweep = SonarSweep()
+        assertEquals(1121, sonarSweep.countDepthIncreasesBest(input, 1))
+    }
+
+    @Test
+    fun countDepthIncreasesGroup3Best() {
+        val input = PuzzleInputTestUtils.getPuzzleInputLinesIntList("day1.txt")
+
+        val sonarSweep = SonarSweep()
+        assertEquals(1065, sonarSweep.countDepthIncreasesBest(input, 3))
+    }
+
 }
