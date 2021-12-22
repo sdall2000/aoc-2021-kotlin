19bbc4e2b7565902211523e2bdf22d770777160b
stephen.dall <sdall@ctic.us>
2021-12-22 13:12:32 -0800

    WIP day 22 adding intersection logic

--- src/main/kotlin/aoc2021/day22/ReactorReboot.kt
+++ src/main/kotlin/aoc2021/day22/ReactorReboot.kt
@@ -39,6 +39,9 @@ class ReactorReboot {
     fun countOnCubes2(lines:List<String>, maxValue:Int? = null): Long {
         val coordinateSet = mutableSetOf<String>()
 
+        val onCubes = mutableListOf<Cube>()
+        val offCubes = mutableListOf<Cube>()
+
         lines.forEach { line ->
             val turnOn = line.startsWith("on")
 
@@ -50,21 +53,46 @@ class ReactorReboot {
             val (minY, maxY) = buildRange(yStr.substringAfter("y="))
             val (minZ, maxZ) = buildRange(zStr.substringAfter("z="))
 
-            if (maxValue == null || inRange(maxValue, intArrayOf(minX, maxX, minY, maxY, minZ, maxZ))) {
-                for (x in minX..maxX) {
-                    for (y in minY..maxY) {
-                        for (z in minZ..maxZ) {
-                            val key = buildPositionKey(x, y, z)
-                            if (turnOn) {
-                                coordinateSet.add(key)
-                            } else {
-                                coordinateSet.remove(key)
-                            }
-                        }
-                    }
+            val cube = Cube(minX, maxX, minY, maxY, minZ, maxZ)
+
+            if (turnOn) {
+                onCubes.add(cube)
+            } else {
+                offCubes.add(cube)
+            }
+        }
+
+        val maxOnCube = onCubes.maxByOrNull {
+            it.size()
+        }
+
+        val maxOffCube = offCubes.maxByOrNull {
+            it.size()
+        }
+
+        onCubes.forEach {
+            if (it != maxOnCube) {
+                if (maxOnCube!!.intersects(it)) {
+                    println("On cube $maxOnCube intersects cube $it.  Intersection is ${maxOnCube.intersect(it)}")
+                }
+
+                if (maxOffCube!!.intersects(it)) {
+                    println("Off cube $maxOffCube intersects cube $it.  Intersection is ${maxOffCube.intersect(it)}")
                 }
             }
         }
+
+//        offCubes.forEach {
+//            if (it != maxOffCube) {
+//                if (maxOffCube!!.intersects(it)) {
+//                    println("Off cube $maxOffCube intersects cube $it.  Intersection is ${maxOffCube.intersect(it)}")
+//                }
+//
+//                if (maxOnCube!!.intersects(it)) {
+//                    println("On cube $maxOnCube intersects cube $it.  Intersection is ${maxOnCube.intersect(it)}")
+//                }
+//            }
+//        }
 
         return coordinateSet.size.toLong()
     }
@@ -97,6 +125,40 @@ class ReactorReboot {
     }
 }
 
-data class Cube(val minX:Long, val maxX: Long, val minY: Long, val maxY: Long, val minZ: Long, val maxZ: Long) {
-    fun size():Long = (maxX - minX) * (maxY - minY) * (maxZ - minZ)
+data class Cube(val minX:Int, val maxX: Int, val minY: Int, val maxY: Int, val minZ: Int, val maxZ: Int) {
+    fun size():Int = (maxX - minX) * (maxY - minY) * (maxZ - minZ)
+
+    // Returns true if the other cube is inside this cube.  matching edges count as well.
+    fun contains(other:Cube): Boolean {
+        return other.minX in minX..maxX && other.maxX in minX..maxX &&
+                other.minY in minY..maxY && other.maxY in minY..maxY &&
+                other.minZ in minZ..maxZ && other.maxZ in minZ..maxZ
+    }
+
+    // Returns true if the other cube intersects this cube.  Edges that touch do not count.
+    fun intersects(other:Cube): Boolean {
+        return other.minX < maxX && other.maxX > minX &&
+                other.minY < maxY && other.maxY > minY &&
+                other.minZ < maxZ && other.maxZ > minZ
+    }
+
+    fun intersect(other:Cube): Cube? {
+        var intersection:Cube? = null
+
+        if (intersects(other)) {
+            // Get the maximum min value
+            val newMinX = minX.coerceAtLeast(other.minX)
+            val newMinY = minY.coerceAtLeast(other.minY)
+            val newMinZ = minZ.coerceAtLeast(other.minZ)
+
+            // Get the minimum max value
+            val newMaxX = maxX.coerceAtMost(other.maxX)
+            val newMaxY = maxY.coerceAtMost(other.maxY)
+            val newMaxZ = maxZ.coerceAtMost(other.maxZ)
+
+            intersection = Cube(newMinX, newMaxX, newMinY, newMaxY, newMinZ, newMaxZ)
+        }
+
+        return intersection
+    }
 }
