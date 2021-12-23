41edcf85de916f52e822a4021c20748f1a4bfd54
Steve Dall <sdall2000@gmail.com>
2021-12-22 23:42:00 -0800

    Day 22 work in progress

--- src/main/kotlin/aoc2021/day22/ReactorReboot.kt
+++ src/main/kotlin/aoc2021/day22/ReactorReboot.kt
@@ -3,7 +3,7 @@ package aoc2021.day22
 import kotlin.math.abs
 
 class ReactorReboot {
-    fun countOnCubes(lines:List<String>, maxValue:Int? = null): Long {
+    fun countOnCubes(lines: List<String>, maxValue: Int? = null): Long {
         val coordinateSet = mutableSetOf<String>()
 
         lines.forEach { line ->
@@ -36,10 +36,10 @@ class ReactorReboot {
         return coordinateSet.size.toLong()
     }
 
-    fun countOnCubes2(lines:List<String>, maxValue:Int? = null): Long {
+    fun countOnCubes2(lines: List<String>, maxValue: Int? = null): Long {
         val coordinateSet = mutableSetOf<String>()
 
-        val onCubes = mutableListOf<Cube>()
+        var onCubes = mutableListOf<Cube>()
         val offCubes = mutableListOf<Cube>()
 
         lines.forEach { line ->
@@ -62,38 +62,49 @@ class ReactorReboot {
             }
         }
 
-        val maxOnCube = onCubes.maxByOrNull {
-            it.size()
-        }
+        // First, rebuild the on cubes so there are no more intersections.
+        println("On cube original size=${onCubes.size}")
 
-        val maxOffCube = offCubes.maxByOrNull {
-            it.size()
-        }
+        var done = false
 
-        onCubes.forEach {
-            if (it != maxOnCube) {
-                if (maxOnCube!!.intersects(it)) {
-                    println("On cube $maxOnCube intersects cube $it.  Intersection is ${maxOnCube.intersect(it)}")
+        while (!done) {
+            val cubes = mutableListOf<Cube>()
+            cubes.addAll(onCubes)
+
+            done = true
+
+            onCubes.forEach { onCube ->
+                onCubes.filter { onCube2 ->
+                    onCube != onCube2 && onCube.intersects(onCube2)
+                }.forEach { onCube3 ->
+                    if (done) {
+                        if (!cubes.remove(onCube)) {
+                            println("Failed to remove cube $onCube")
+                        }
+                        val newCubes = onCube.remove(onCube3)
+                        if (newCubes.isNotEmpty()) {
+                            cubes.addAll(newCubes)
+                            println("Updated on cubes size: ${cubes.size}")
+
+                            println("originalCube: $onCube")
+                            println("intersectingCube: $onCube3")
+                            println("intersection: ${onCube.intersect(onCube3)}")
+                            println("New cubes: ")
+                            newCubes.forEach { c ->
+                                println("  $c")
+                            }
+                        }
+                        done = false
+                    }
                 }
+            }
 
-                if (maxOffCube!!.intersects(it)) {
-                    println("Off cube $maxOffCube intersects cube $it.  Intersection is ${maxOffCube.intersect(it)}")
-                }
+            if (!done) {
+                onCubes = cubes
             }
         }
 
-//        offCubes.forEach {
-//            if (it != maxOffCube) {
-//                if (maxOffCube!!.intersects(it)) {
-//                    println("Off cube $maxOffCube intersects cube $it.  Intersection is ${maxOffCube.intersect(it)}")
-//                }
-//
-//                if (maxOnCube!!.intersects(it)) {
-//                    println("On cube $maxOnCube intersects cube $it.  Intersection is ${maxOnCube.intersect(it)}")
-//                }
-//            }
-//        }
-
+        println("On cube new size=${onCubes.size}")
         return coordinateSet.size.toLong()
     }
 
@@ -115,7 +126,7 @@ class ReactorReboot {
         return "$x/$y/$z"
     }
 
-    private fun inRange(maxValue: Int, number:IntArray): Boolean {
+    private fun inRange(maxValue: Int, number: IntArray): Boolean {
         number.forEach {
             if (abs(it) > maxValue) {
                 return false
@@ -125,25 +136,25 @@ class ReactorReboot {
     }
 }
 
-data class Cube(val minX:Int, val maxX: Int, val minY: Int, val maxY: Int, val minZ: Int, val maxZ: Int) {
-    fun size():Int = (maxX - minX) * (maxY - minY) * (maxZ - minZ)
+data class Cube(val minX: Int, val maxX: Int, val minY: Int, val maxY: Int, val minZ: Int, val maxZ: Int) {
+    fun size(): Int = (maxX - minX) * (maxY - minY) * (maxZ - minZ)
 
     // Returns true if the other cube is inside this cube.  matching edges count as well.
-    fun contains(other:Cube): Boolean {
+    fun contains(other: Cube): Boolean {
         return other.minX in minX..maxX && other.maxX in minX..maxX &&
                 other.minY in minY..maxY && other.maxY in minY..maxY &&
                 other.minZ in minZ..maxZ && other.maxZ in minZ..maxZ
     }
 
     // Returns true if the other cube intersects this cube.  Edges that touch do not count.
-    fun intersects(other:Cube): Boolean {
-        return other.minX < maxX && other.maxX > minX &&
-                other.minY < maxY && other.maxY > minY &&
-                other.minZ < maxZ && other.maxZ > minZ
+    fun intersects(other: Cube): Boolean {
+        return other.minX <= maxX && other.maxX >= minX &&
+                other.minY <= maxY && other.maxY >= minY &&
+                other.minZ <= maxZ && other.maxZ >= minZ
     }
 
-    fun intersect(other:Cube): Cube? {
-        var intersection:Cube? = null
+    fun intersect(other: Cube): Cube? {
+        var intersection: Cube? = null
 
         if (intersects(other)) {
             // Get the maximum min value
@@ -160,5 +171,68 @@ data class Cube(val minX:Int, val maxX: Int, val minY: Int, val maxY: Int, val m
         }
 
         return intersection
+    }
+
+    // Returns one or more cubes after removing the given cube
+    // May return an empty list if the cube passed in as a parameter contains this cube object.
+    fun remove(cube: Cube): List<Cube> {
+        val cubeList = mutableListOf<Cube>()
+
+        // First get the intersection
+        val intersectCube = intersect(cube)
+
+        // See if an intersection exists
+        if (intersectCube != null) {
+            // Now, we need to subtract the intersect cube from this cube.
+            // Many use cases:
+            //    The intersect cube contains this cube.  So no cube is left and we will return an empty list.
+            //
+
+            // Create up to six new cubes.
+
+            // Default to current cube values.
+            var newMinY = minY
+            var newMaxY = maxY
+            var newMinX = minX
+            var newMaxX = maxX
+
+            // This is the area above the intersection.
+            if (intersectCube.maxY < maxY) {
+                cubeList.add(Cube(minX, maxX, intersectCube.maxY, maxY, minZ, maxZ))
+                newMaxY = intersectCube.maxY
+            }
+
+            // Area below the intersection
+            if (intersectCube.minY > minY) {
+                cubeList.add(Cube(minX, maxX, minY, intersectCube.minY, minZ, maxZ))
+                newMinY = intersectCube.minY
+            }
+
+            // Area to the right of the intersection
+            if (intersectCube.maxX < maxX) {
+                cubeList.add(Cube(intersectCube.maxX, maxX, newMinY, newMaxY, minZ, maxZ))
+                newMaxX = intersectCube.maxX
+            }
+
+            // Area to the left of the intersection
+            if (intersectCube.minX > minX) {
+                cubeList.add(Cube(minX, intersectCube.minX, newMinY, newMaxY, minZ, maxZ))
+                newMinX = intersectCube.minX
+            }
+
+            if (intersectCube.maxZ < maxZ) {
+                cubeList.add(Cube(newMinX, newMaxX, newMinY, newMaxY, intersectCube.maxZ, maxZ))
+            }
+
+            if (intersectCube.minZ > minZ) {
+                cubeList.add(Cube(newMinX, newMaxX, newMinY, newMaxY, minZ, intersectCube.minZ))
+            }
+        } else {
+            // There is no intersection.  Return a copy of this cube.
+            // (Could we just add "this" instead of a copy?)
+            cubeList.add(this.copy())
+        }
+
+        return cubeList
     }
 }
